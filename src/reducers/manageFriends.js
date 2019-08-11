39c5ad168bcfec5d1f6afbf9db1eba341469d1d6
Amy Cen <amy.cen@blackrock.com>
2019-08-11 03:13:37 +0000

    Automatically backed up by Learn

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,7 +1,7 @@
 export function manageFriends(state, action){
   switch(action.type){
     case 'ADD_FRIEND':
-      return {...state, friends: [...state.friends, {name: action.friend.name, hometown: action.friend.hometown, id: actionfriend..id}]}
+      return {...state, friends: [...state.friends, {name: action.friend.name, hometown: action.friend.hometown, id: actionfriend.id}]}
     default:
       state
   }
