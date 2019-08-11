a4684284cc209e56c9050ccf89bd5a5b7d6eae2a
Amy Cen <amy.cen@blackrock.com>
2019-08-11 03:06:50 +0000

    Automatically backed up by Learn

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,7 +1,8 @@
 export function managePresents(state, action){
-    switch(action.type)
-    case "INCREASE"
+    switch(action.type){
+    case "INCREASE":
       return {...state, state.numberOfPresents + 1}
     default:
       return state
+    }
 }
